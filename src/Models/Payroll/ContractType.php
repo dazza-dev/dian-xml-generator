746844388ba224488038cddd8b61f472942ff6d3
Dazza <adaza90@outlook.com>
2025-10-02 13:11:26 -0500

    Base Model

--- src/Models/Payroll/ContractType.php
+++ src/Models/Payroll/ContractType.php
@@ -2,75 +2,15 @@
 
 namespace DazzaDev\DianXmlGenerator\Models\Payroll;
 
-class ContractType
+use DazzaDev\DianXmlGenerator\Models\BaseModel;
+
+class ContractType extends BaseModel
 {
-    /**
-     * Contract type code
-     */
-    private string $code;
-
-    /**
-     * Contract type name
-     */
-    private string $name;
-
-    /**
-     * Contract type constructor
-     */
-    public function __construct(array $data = [])
-    {
-        $this->initialize($data);
-    }
-
-    /**
-     * Initialize contract type data
-     */
-    private function initialize(array $data): void
-    {
-        $this->setCode($data['code']);
-        $this->setName($data['name']);
-    }
-
-    /**
-     * Get contract type code
-     */
-    public function getCode(): string
-    {
-        return $this->code;
-    }
-
-    /**
-     * Set contract type code
-     */
-    private function setCode(string $code): void
-    {
-        $this->code = $code;
-    }
-
-    /**
-     * Get contract type name
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set contract type name
-     */
-    private function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
     /**
      * Get array representation
      */
     public function toArray(): array
     {
-        return [
-            'code' => $this->code,
-            'name' => $this->name,
-        ];
+        return $this->getBaseArray();
     }
 }
--- src/Models/Tax/TaxType.php
+++ src/Models/Tax/TaxType.php
@@ -2,79 +2,15 @@
 
 namespace DazzaDev\DianXmlGenerator\Models\Tax;
 
-class TaxType
+use DazzaDev\DianXmlGenerator\Models\BaseModel;
+
+class TaxType extends BaseModel
 {
     /**
-     * Tax Code
-     */
-    private string $code;
-
-    /**
-     * Tax Name
-     */
-    private string $name;
-
-    /**
-     * Tax Type constructor
-     */
-    public function __construct(array $data = [])
-    {
-        $this->initialize($data);
-    }
-
-    /**
-     * Initialize tax data
-     */
-    private function initialize(array $data): void
-    {
-        if (empty($data)) {
-            return;
-        }
-
-        $this->setCode($data['code']);
-        $this->setName($data['name']);
-    }
-
-    /**
-     * Get code
-     */
-    public function getCode(): string
-    {
-        return $this->code;
-    }
-
-    /**
-     * Set code
-     */
-    public function setCode(string $code): void
-    {
-        $this->code = trim($code);
-    }
-
-    /**
-     * Get percent
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set percent
-     */
-    public function setName(string $name): void
-    {
-        $this->name = trim($name);
-    }
-
-    /**
-     * Convert tax type to array
+     * Get array representation
      */
     public function toArray(): array
     {
-        return [
-            'code' => $this->code,
-            'name' => $this->name,
-        ];
+        return $this->getBaseArray();
     }
 }
